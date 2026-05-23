c54e578df9c1f343ab1421ffaa3bf53b6beeb584
o1-spec <oluwafemionadokun@gmail.com>
2026-05-23 12:57:38 +0100

    added search

--- src/modules/messages/messages.module.ts
+++ src/modules/messages/messages.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { MessagesController } from './messages.controller.js';
 import { MessagesService } from './messages.service.js';
+import { MessagesGateway } from './messages.gateway.js';
 
 @Module({
+  imports: [JwtModule.register({})],
   controllers: [MessagesController],
-  providers: [MessagesService],
+  providers: [MessagesService, MessagesGateway],
   exports: [MessagesService],
 })
 export class MessagesModule {}
